c4909a6230a6f2e387fc403015d7606ba54f6e83
prokofieva <prokofieva@cs.columbia.edu>
2016-01-06 17:45:31 -0500

    Done: added input arguments to limit how many books to download, whether to just download or generate counts files on the fly and to specify the file from which to read urls to be downloaded.

--- get_files.py
+++ get_files.py
@@ -3,47 +3,84 @@ import urllib
 import zipfile
 import tfidf
 import os
+import sys
 from os.path import join
 from bs4 import BeautifulSoup
 
+#So now you can either download in a batch or download one by one and generate the counts files on the fly (if downloading very large amounts of text)
 #TODO:
-#make count input argument (how many books to process)
+#X:make count input argument (how many books to process)
 #add support for special chars - i.e. translate weird chards to English
-#separate out so we can just download the files and get the .txt versus processing everything
+#X:separate out so we can just download the files and get the .txt versus processing everything
+#X:add option to read initial URL from file (so I can have my own list of URLs for toy example with genres)
+#http://www.gutenberg.org/robot/harvest?filetypes[]=txt&langs[]=en
 
-response = urllib2.urlopen('http://www.gutenberg.org/robot/harvest?filetypes[]=txt&langs[]=en')
-html = response.read()
+#how many books to process
+maxcount = int(sys.argv[1])
+#option is to either download or all
+mode = sys.argv[2]
+#file which contains the URL to mine (urls.md - don't save as txt file or program will attempt to read it during the analysis)
+urlfile = sys.argv[3]
 
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.find_all('a')
-count = 0      
-for link in links:
-    if count < 5:
-        url = link.get('href')
-        tmp_fn = url.split("/")
-        filename = tmp_fn[len(tmp_fn)-1]
-        if ("zip" in url and "etext" not in url and "-8" not in url):
-            urllib.urlretrieve(url, filename)
-            zip = zipfile.ZipFile(filename)
-            zip.extractall()
-            txt_filename = filename.replace(".zip", ".txt")
-            if os.path.isfile(txt_filename):
-                tfidf.getWordCounts(txt_filename)
-            elif os.path.isdir(txt_filename.replace(".txt","")):
-                actual_filename = join(txt_filename.replace(".txt", ""),txt_filename)
-                tfidf.getWordCounts(actual_filename)
-                os.rmdir(txt_filename.replace(".txt",""))
-            os.remove(filename)
-            count += 1
-        elif "offset" in url:
-            url = "http://www.gutenberg.org/robot/"+url
-            new_response = urllib2.urlopen(url)
-            new_html = new_response.read()
-            new_soup = BeautifulSoup(new_html, 'html.parser')
-            links.extend(new_soup.find_all('a'))
+def processFile(filename):
+    txt_filename = filename.replace(".zip", ".txt")
+    if os.path.isfile(txt_filename):
+        tfidf.getWordCounts(txt_filename)
+    elif os.path.isdir(txt_filename.replace(".txt","")):
+        actual_filename = join(txt_filename.replace(".txt", ""),txt_filename)
+        tfidf.getWordCounts(actual_filename)
+        os.rmdir(txt_filename.replace(".txt",""))
+
+
+def downloadLink(url):
+    tmp_fn = url.split("/")
+    file_name = tmp_fn[len(tmp_fn)-1]
+    
+    #To make sure we're extracting only zip files that in the right format
+    if ("zip" in url and "etext" not in url and "-8" not in url):
+        urllib.urlretrieve(url, file_name)
+        zip = zipfile.ZipFile(file_name)
+        zip.extractall()
+        os.remove(file_name)
+        if "all" in mode:
+            processFile(file_name)
+        else:
+            print file_name
+        return 1
+    else:
+        return 0
         
 
-tfidf.calcTfidf(".")
+def readInUrlFile(url_file):
+    with open(url_file, 'rU') as u_file:
+        cn = 0
+        for line in u_file:
+            if "robot" in line:
+                response = urllib2.urlopen(line)
+                html = response.read()
+                soup = BeautifulSoup(html, 'html.parser')
+                links = soup.find_all('a')
+                for link in links:
+                    if cn < maxcount:
+                        link_url = link.get('href')
+                        if "offset" in link_url:
+                            link_url = "http://www.gutenberg.org/robot/"+link_url
+                            new_response = urllib2.urlopen(url)
+                            new_html = new_response.read()
+                            new_soup = BeautifulSoup(new_html, 'html.parser')
+                            links.extend(new_soup.find_all('a'))
+                        else:
+                            cn += downloadLink(link_url)
+            else:
+                if cn < maxcount:
+                    cn += downloadLink(line)
+
+def main():
+    readInUrlFile(urlfile)
+
+if __name__ == "__main__":
+    main()
+
 
 
 
